3e60d5c0893c3dfa35aeebf51ee7e8280a4c22cb
Stefan Fisk <contact@stefanfisk.com>
2024-05-15 14:17:20 +0200

    Fix hero-1 init function name

--- src/blocks/hero-1/index.php
+++ src/blocks/hero-1/index.php
@@ -1,6 +1,6 @@
 <?php
 
-function ztk_register__block() {
+function ztk_register_hero_1_block() {
 	ztk_register_block_type_from_metadata( __DIR__ );
 }
-add_action( 'init', 'ztk_register__block' );
+add_action( 'init', 'ztk_register_hero_1_block' );
