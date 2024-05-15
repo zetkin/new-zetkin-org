6d0e395741db442ed177acdd068b316f842cfd3e
Stefan Fisk <contact@stefanfisk.com>
2024-05-15 13:44:46 +0200

    Make pin-out format use fixed palette

--- src/formats/pin-out/index.js
+++ src/formats/pin-out/index.js
@@ -3,19 +3,135 @@
  */
 import { __ } from '@wordpress/i18n';
 import { useCallback, useMemo, useState } from '@wordpress/element';
-import { useSettings } from '@wordpress/block-editor';
-import { Fill, ToolbarButton } from '@wordpress/components';
-import { removeFormat } from '@wordpress/rich-text';
+import {
+	getColorObjectByAttributeValues,
+	getColorObjectByColorValue,
+	ColorPalette,
+} from '@wordpress/block-editor';
+import { Fill, Popover, ToolbarButton } from '@wordpress/components';
+import {
+	applyFormat,
+	getActiveFormat,
+	removeFormat,
+	useAnchor,
+} from '@wordpress/rich-text';
 
 /**
  * Internal dependencies
  */
+import { baseColors } from '../../scripts/colors';
 import { ReactComponent as Icon } from './icon.svg';
-import { default as InlineColorUI, getActiveColor } from './inline';
 
 export const name = 'zetkin/pin-out';
 
-const title = __( 'Zetkin Pin-Out' );
+const TITLE = __( 'Zetkin Pin-Out' );
+
+const CLASS_PREFIX = 'has-';
+const CLASS_SUFFIX = '-zetkin-pin-out';
+
+function isColorClassName( c ) {
+	return c.startsWith( CLASS_PREFIX ) && c.endsWith( CLASS_SUFFIX );
+}
+
+function getColorClassNameSlug( c ) {
+	return c.substring( CLASS_PREFIX.length, c.length - CLASS_SUFFIX.length );
+}
+
+function parseClassName( className = '' ) {
+	let color = null;
+
+	className.split( ' ' ).some( ( c ) => {
+		if ( ! isColorClassName( c ) ) {
+			return false;
+		}
+
+		const colorSlug = getColorClassNameSlug( c );
+
+		const colorObject = getColorObjectByAttributeValues(
+			baseColors,
+			colorSlug
+		);
+
+		color = colorObject?.color;
+
+		return !! color;
+	} );
+
+	return color;
+}
+
+function getActiveColor( value ) {
+	const activeColorFormat = getActiveFormat( value, name );
+
+	if ( ! activeColorFormat ) {
+		return null;
+	}
+
+	return parseClassName( activeColorFormat.attributes.class );
+}
+
+function setColor( value, color ) {
+	if ( ! color ) {
+		return removeFormat( value, name );
+	}
+
+	const classNames = [];
+	const attributes = {};
+
+	if ( color ) {
+		const colorObject = getColorObjectByColorValue( baseColors, color );
+
+		if ( colorObject ) {
+			classNames.push( CLASS_PREFIX + colorObject.slug + CLASS_SUFFIX );
+		}
+	}
+
+	if ( classNames.length ) {
+		attributes.class = classNames.join( ' ' );
+	}
+
+	return applyFormat( value, { type: name, attributes } );
+}
+
+function PinOutFormatColorPicker( props ) {
+	const { value, onChange } = props;
+
+	const onColorChange = useCallback(
+		( color ) => {
+			onChange( setColor( value, color ) );
+		},
+		[ value, onChange ]
+	);
+
+	const activeColor = useMemo( () => getActiveColor( value ), [ value ] );
+
+	return (
+		<ColorPalette
+			colors={ baseColors }
+			value={ activeColor }
+			onChange={ onColorChange }
+		/>
+	);
+}
+
+function PinOutFormatInlineEdit( {
+	value,
+	onChange,
+	onClose,
+	contentRef,
+	isActive,
+} ) {
+	const popoverAnchor = useAnchor( {
+		editableContentElement: contentRef.current,
+		settings: { ...settings, isActive },
+	} );
+
+	return (
+		<Popover onClose={ onClose } anchor={ popoverAnchor }>
+			<PinOutFormatColorPicker value={ value } onChange={ onChange } />
+		</Popover>
+	);
+}
 
 function PinOutFormatEdit( {
 	value,
@@ -24,8 +140,6 @@ function PinOutFormatEdit( {
 	activeAttributes,
 	contentRef,
 } ) {
-	const [ colors ] = useSettings( 'color.palette' );
-
 	const [ isAddingColor, setIsAddingColor ] = useState( false );
 
 	const enableIsAddingColor = useCallback(
@@ -40,17 +154,11 @@ function PinOutFormatEdit( {
 
 	const colorIndicatorStyle = useMemo(
 		() => ( {
-			fill: getActiveColor( value, name, colors ),
+			fill: getActiveColor( value ),
 			background: 'white',
 		} ),
-		[ value, colors ]
+		[ value ]
 	);
-
-	const hasColorsToChoose = !! colors.length;
-
-	if ( ! hasColorsToChoose && ! isActive ) {
-		return null;
-	}
 
 	return (
 		<>
@@ -59,20 +167,15 @@ function PinOutFormatEdit( {
 					className="format-library-text-color-button"
 					isActive={ isActive }
 					icon={ <Icon style={ colorIndicatorStyle } /> }
-					title={ title }
+					title={ TITLE }
 					// If has no colors to choose but a color is active remove the color onClick.
-					onClick={
-						hasColorsToChoose
-							? enableIsAddingColor
-							: () => onChange( removeFormat( value, name ) )
-					}
+					onClick={ enableIsAddingColor }
 					role="menuitemcheckbox"
 				/>
 			</Fill>
 
 			{ isAddingColor && (
-				<InlineColorUI
-					name={ name }
+				<PinOutFormatInlineEdit
 					onClose={ disableIsAddingColor }
 					activeAttributes={ activeAttributes }
 					value={ value }
@@ -85,9 +188,9 @@ function PinOutFormatEdit( {
 	);
 }
 
-export default {
+const settings = {
 	name,
-	title,
+	title: TITLE,
 	tagName: 'span',
 	className: 'has-zetkin-pin-out',
 	attributes: {
@@ -96,3 +199,5 @@ export default {
 	},
 	edit: PinOutFormatEdit,
 };
+
+export default settings;
---
+++ src/scripts/colors.js
@@ -0,0 +1,44 @@
+export const black = {
+	slug: 'black',
+	color: 'var(--ztk--color--black)',
+	name: 'Black',
+};
+
+export const white = {
+	slug: 'white',
+	color: 'var(--ztk--color--white)',
+	name: 'White',
+};
+
+export const red500 = {
+	slug: 'red-500',
+	color: 'var(--ztk--color--red-500)',
+	name: 'Red',
+};
+
+export const lightGreen500 = {
+	slug: 'light-green-500',
+	color: 'var(--ztk--color--light-green-500)',
+	name: 'Light Green',
+};
+
+export const darkGreen500 = {
+	slug: 'dark-green-500',
+	color: 'var(--ztk--color--dark-green-500)',
+	name: 'Dark Green',
+};
+
+export const purple500 = {
+	slug: 'purple-500',
+	color: 'var(--ztk--color--purple-500)',
+	name: 'Purple',
+};
+
+export const baseColors = [
+	black,
+	white,
+	red500,
+	lightGreen500,
+	darkGreen500,
+	purple500,
+];
